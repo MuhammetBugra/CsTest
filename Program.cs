b1ef433bab8f940cbf71d0e8885473b05a232796
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:27:37 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+namespace getsetclass {
+    class exponentor { 
+    private int number;
+    private int exponent; 
+    public int Number { set { number = Number; } get { return number; } }
+    public exponentor(int exponent) {
+    this.exponent = exponent; }
+    public void printexp() { 
+    Console.WriteLine(Math.Pow(Number,exponent)); } }
+    internal class Program {
+    static void Main(string[] args) {
+    int a;
+    Console.Write("us: ");
+    a=Convert.ToInt32(Console.ReadLine());
+    exponentor exp=new exponentor(a);
+    Console.Write("sayi: ");
+    exp.Number = Convert.ToInt32(Console.ReadLine());
+    exp.printexp(); } } }
