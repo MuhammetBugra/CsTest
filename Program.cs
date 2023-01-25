2fa3f210ee8a9214b0cca56e9354c94569300cb9
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:24:39 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace dizisiralama
+{
+    internal class Program {
+    static void Main(string[] args) {
+    int[] dizi=new int[5];
+    for(int i=0; i<5; i++) {
+    Console.Write("sayi: ");
+    dizi[i]=Convert.ToInt32(Console.ReadLine()); }
+    Array.Sort(dizi);
+    foreach(int i in dizi) {
+    Console.Write(i+" "); }
+    Console.WriteLine();
+    Console.WriteLine("min: "+dizi[0]);
+    Console.WriteLine("max: "+dizi[4]);
+    Console.WriteLine("boyut: "+dizi.Length);
+    Array.Reverse(dizi);
+    foreach(int i in dizi) {
+    Console.Write(i+" "); }
+} } }
