cd523d07428a12fbbf4e37cfd069e2af927a555b
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:06:57 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Text.RegularExpressions;
+
+namespace altstring {
+    public class RegularExpressionsExample {
+    static void Main(string[] args) {
+    string pattern=@"\w+";
+    Regex rgx = new Regex(pattern);
+    string input=Console.ReadLine();
+    string[] str1=rgx.Split(input);
+    foreach(string s in str1){
+    Console.Write(s);
+    Console.WriteLine(); }
+} } }
