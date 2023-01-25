f444d0600688a48ffeab030b19be3cccafce28e5
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:10:08 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace baklavadilimi {
+    internal class Program {
+    static void Main(string[] args) {
+    int n;
+    Console.Write("sayi: ");
+    n=Convert.ToInt32(Console.ReadLine());
+    for(int i = 0; i < n; i++) {
+    for(int j = 0; j < (n-i); j++) {
+    Console.Write(" "); }
+    for(int k = 0; k < 2*i; k++) {
+    Console.Write("*"); }
+    Console.WriteLine(); }
+    for(int i = 0; i < n; i++) {
+    for(int j = 0; j < i; j++) {
+    Console.Write(" "); }
+    for(int k = 0; k < 2*(n-i); k++) {
+    Console.Write("*"); }
+    Console.WriteLine(); } } } }
