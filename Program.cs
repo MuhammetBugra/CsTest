b9c1afdc569c05584685067d4e8bd4eefd749f6e
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:27:58 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace harfbulma {
+    internal class Program {
+    static void Main(string[] args) {
+    string s= "BM102 Bilgisayar Programlama II Lab Uygulaması";
+    Console.WriteLine("harf girin: ");
+    s=s.ToLower();
+    char c=Convert.ToChar(Console.ReadLine());
+    int sayi=0;
+    for(int i=0;i<s.Length;i++) {
+    if(c==s[i]) {
+    sayi ++; } }
+    Console.WriteLine(s);
+    Console.WriteLine("cumlede "+c+" harfinden "+sayi+" tane var."); } } } 
