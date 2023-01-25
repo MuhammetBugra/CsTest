b913460735c7ccdde9a14bfe999ae33a0f00d523
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:12:12 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace carpimtablosu {
+    internal class Program {
+    static void Main(string[] args) {
+    int i,j;
+    for(i=1;i<10;i++) { 
+    for(j=1;j<10;j++) {
+    Console.Write("{0}*{1}=",i,j);
+    Console.Write((i*j)+" "); }
+    Console.WriteLine(); } } } }
