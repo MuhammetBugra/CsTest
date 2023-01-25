03c1997b5566031626228a4d54ff0c7c3789b664
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:11:26 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace bool1 {
+    internal class Program {
+    static void Main(string[] args) {
+    int sifre;
+    Console.Write("sifre:");
+    sifre = Convert.ToInt32(Console.ReadLine());
+    bool durum=sifre==1234;
+    Console.WriteLine("sifre dogru mu: {0}",durum); } } }
