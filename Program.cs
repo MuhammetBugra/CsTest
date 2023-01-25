16118bd2cd567a3e97e150df5a3826c3146c4a49
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:11:48 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace byteshort {
+    internal class Program {
+    static void Main(string[] args) {
+    byte a; sbyte b;
+    short c; short d;
+    a=255; b = 0;
+    c = 32767; d = -32768;
+    Console.WriteLine(a+" "+b+" "+c+" "+d); } } }
