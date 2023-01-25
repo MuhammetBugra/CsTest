67a1ef52d7693bf62f5da3f3eae1ea57048db737
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:10:28 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace basamakayirma {
+    internal class Program {
+    static void Main(string[] args) {
+    int n,birler,onlar,yuzler;
+    Console.WriteLine("Sayi Giriniz: ");
+    n=Convert.ToInt32(Console.ReadLine());
+    birler=n%10;
+    onlar=(n/10)%10;
+    yuzler=(n/100)%10;
+    Console.WriteLine("birler:{0} onlar:{1} yuzler:{2} ",birler,onlar,yuzler); } } }
