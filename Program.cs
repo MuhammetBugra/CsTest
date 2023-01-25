415adc8bfc3c3a611c8ac9fe334fe4c6e5a03b10
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:08:02 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace arkaplan {
+    internal class Program {
+    static void Main(string[] args) {
+    Console.BackgroundColor = ConsoleColor.Yellow;
+    Console.ForegroundColor = ConsoleColor.DarkBlue;
+    Console.Title = "Sehirler";
+    Console.Clear();
+    Console.WriteLine("Ankara");
+    Console.Read(); } } }
