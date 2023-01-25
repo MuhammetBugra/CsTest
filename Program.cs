c7b365bc513246684af00755d505042e84e7438f
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:28:44 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ifmod {
+    internal class Program {
+    static void Main(string[] args) {
+    int n;
+    Console.Write("Sayi: ");
+    n=Convert.ToInt32(Console.ReadLine());
+    if(n%2==0) { 
+    Console.WriteLine("Sayi cifttir."); }
+    else { 
+    Console.WriteLine("Sayi tektir."); } } } }
