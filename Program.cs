82c2d5402ffa53fd59ea50fcf5e538735db446f9
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:08:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace asalrecursive {
+    internal class Program {
+    static void Main(string[] args) {
+    int n; int[] dizi=new int[3];
+    n=Convert.ToInt32(Console.ReadLine());
+    for(int i = 0; i < 3; i++) {
+    dizi[i]=fonksiyon(n);
+    Console.WriteLine(dizi[i]+" asaldir."); } }
+    static int fonksiyon(int n) {
+    int a=0;
+    for(int i = 2; i < n; i++) {
+    if(n%i== 0) {
+    a=1; } }
+    if(a==1) {
+    return fonksiyon(n + 1); }
+    else {
+    return n; } } } }
