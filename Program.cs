ab1ebb8792a2b4a2ad3c866e8b8965aa4c76d312
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:26:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace faktoriyel {
+    internal class Program {
+    static void Main(string[] args) {
+    int n,fak=1;
+    Console.Write("sayi: ");
+    n=Convert.ToInt32(Console.ReadLine());
+    while(n>1) { 
+    fak=fak*n;
+    n--; }
+    Console.WriteLine("sonuc: "+fak); } } }
