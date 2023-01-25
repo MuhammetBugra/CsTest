821dbfb0d951fb2af197706fe807fdc3b060ae70
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:23:40 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace dikdortgen {
+    internal class Program {
+    static void Main(string[] args) {
+    int a,b;
+    Console.Write("uzun: ");
+    a=Convert.ToInt32(Console.ReadLine());
+    Console.Write("kisa: ");
+    b=Convert.ToInt32(Console.ReadLine());
+    for(int i=0;i<a;i++) {
+    for(int j=0;j<b;j++) {
+    if(i==0 || j==0 || i==(a-1) || j==(b-1)) {
+    Console.Write(" *"); }
+    else {
+    Console.Write("  "); } }
+    Console.WriteLine(); } } } } 
