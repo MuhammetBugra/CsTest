b47e46de8de25a6aa8fa586b594859cfd4a2df51
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:15:46 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+
+namespace constpi
+{
+    internal class Program {
+    static void Main(string[] args) {
+    const double pi=3.14;
+    int sayi;
+    Console.Write("Sayi: ");
+    sayi=Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("sonuc: "+(sayi/pi)); } } }
