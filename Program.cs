4d28e73bf3ce6102cba00001abea1108d2fd335b
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:15:24 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace cokbicimlilik {
+    class insan { 
+    public virtual void selamver() {
+    Console.BackgroundColor=ConsoleColor.Blue;
+    Console.Title="ornek";
+    Console.WriteLine("merhaba"); } }
+    class Turk:insan {
+    public override void selamver() {
+    Console.WriteLine("esenlikler"); } }
+    class fransa:insan {
+    public override void selamver() {
+     Console.BackgroundColor=ConsoleColor.Red;
+    Console.WriteLine("bonjour"); } }
+    class Program {
+    static void Main(string[] args) {
+    Turk trk=new Turk();
+    trk.selamver();
+    fransa fr=new fransa();
+    fr.selamver();
+} } }
