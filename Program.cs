b65a721e937ad40eb15ddac0bec5fbc9e012f0c7
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:24:04 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace diziort{
+    internal class Program {
+    static void Main(string[] args) {
+    int[] dizi=new int[20];
+    int toplam=0;
+    Random r = new Random();
+    for (int i = 0; i < 10; i++) {
+    dizi[i] = r.Next(0,10);
+    toplam+=dizi[i];
+    Console.Write(" "+dizi[i]); }
+    int min=dizi[0],max=dizi[0];
+    for(int i = 1; i < 10; i++) {
+    if(dizi[i] < min) {
+    min=dizi[i]; }
+    if(dizi[i] > max) {
+    max=dizi[i]; } }
+    Console.WriteLine("\nart ort: "+(toplam/10));
+    Console.WriteLine("min: "+min);
+    Console.WriteLine("max: "+max); } } }
