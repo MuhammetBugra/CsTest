46e85477e432cf7e76b6b32cfcc58b8e8ca3b337
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:08:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace asallik{
+     internal class Program{
+     static void Main(string[] args) {
+     int n;
+     Console.WriteLine("sayi giriniz: ");
+     n=Convert.ToInt32(Console.ReadLine());
+     Console.WriteLine("sonuc: "+f(n));
+     if(f(n)==true) {
+     n=n+1;
+     while(f(n)==false) {
+     n++;
+     f(n); } }
+     Console.WriteLine("{0} girdiginiz sayidan sonraki ilk asal sayidir: ",n); }
+     static bool f(int n) {
+     int i,a=0;
+     for(i=2;i<n;i++) {
+     if(n%i==0) { 
+     a++; } }
+     if(a==0) {
+     return true; }
+     else { 
+     return false; } } } }
