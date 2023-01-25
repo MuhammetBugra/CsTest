dc23ddfdf17ba5d3b6c4b729cd4d8471a19c9f4c
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:29:27 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,9 @@
+﻿using System;
+
+namespace indexofdegeri {
+    internal class Program {
+    static void Main(string[] args) {
+    string metin;
+    metin =Console.ReadLine();
+    int no=metin.IndexOf(" ");
+    Console.WriteLine(metin.Substring(0,no)); } } }
