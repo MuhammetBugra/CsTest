c7116c4c4f8afcae4464d3c6737d969c24487a92
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:26:45 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace foreach1 {
+    internal class Program {
+    static void Main(string[] args) {
+    int[] sayilar = {1,2,3,4,5};
+    int toplam=0;
+    foreach (int i in sayilar) {
+    toplam+=i; }
+    Console.Write("sonuc: "+toplam); } } }
