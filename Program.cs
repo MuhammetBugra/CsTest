b0ddb5ed52e995d5a030a92e5b9c21390a7995d6
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:29:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace indirim
+{
+    internal class Program {
+    static void Main(string[] args) {
+    double sayi,sayi1;
+    Console.Write("Sayi: ");
+    sayi=Convert.ToInt32(Console.ReadLine());
+    Console.Write("Sayi 2: ");
+    sayi1=Convert.ToInt32(Console.ReadLine());
+    if((sayi+sayi1)>200) { 
+    Console.WriteLine("sonuc: "+(sayi+(sayi1*0.75)));  }
+    else { 
+    Console.WriteLine("sonuc: "+(sayi+sayi1)); } } } }
