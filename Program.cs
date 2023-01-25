a1541e809240256b24d879a6b53e39e4be858ea4
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:25:00 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace enbuyuk {
+    internal class Program {
+    static void Main(string[] args) {
+    int[] n=new int[5]; 
+    for(int i = 0; i < 5; i++) {
+    Console.Write("sayi: ");
+    n[i]=Convert.ToInt32(Console.ReadLine()); }
+    int max=n[0];
+    for(int i = 0; i < 5; i++) {
+    if(n[i]>max) {
+    max=n[i]; } }
+    Console.WriteLine("max: "+max); } } }
