61211f266b65f1c3bf614ba11cce8e009bd5236d
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:22:50 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace ders1
+{
+    internal class Program {
+    static void Main(string[] args) {
+    string isim;
+    Console.Write("Ad: ");
+    isim = Console.ReadLine();
+    Console.WriteLine("merhaba "+isim); } } }
