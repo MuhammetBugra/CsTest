9123094da590d8e8e8afda41765dae560b54b50b
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:26:20 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace fibonacci {
+    internal class Program {
+    static void Main(string[] args) {
+    int a=0,b=1,c,n;
+    Console.WriteLine("sayi: ");
+    n=Convert.ToInt32(Console.ReadLine());
+    Console.Write(a+" "+b);
+    for(int i=0;i<n;i++) {
+    c=a+b;
+    a=b;
+    b=c;
+    Console.Write(" "+c); } } } }
