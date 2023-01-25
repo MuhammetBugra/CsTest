0122a196ec2d6f8b42c87a2c335d7599921202a8
Muhammet Buğra Güler <123442932+MuhammetBugra@users.noreply.github.com>
2023-01-25 23:27:16 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace getset {
+    internal class Program {
+    static void Main(string[] args) {
+    ogrenci ogr=new ogrenci();
+    ogr.adi="bugra";
+    ogr.soyadi="guler";
+    ogr.alani="muhendislik";
+    ogr.yasi=20;
+    Console.WriteLine("adi: "+ogr.adi);
+    Console.WriteLine("soyadi: "+ogr.soyadi);
+    Console.WriteLine("alani: "+ogr.alani);
+    Console.WriteLine("yasi: "+ogr.yasi); } } }
---
+++ ogrenci.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace getset {
+    class ogrenci {
+    private string ad;
+    private string soyad;
+    private string alan;
+    private int yas;
+    public string adi { 
+    get { return ad; } set { ad = value; } }
+    public string soyadi { 
+    get { return soyad; } set { soyad = value; } }
+    public string alani { 
+    get { return alan; } set { alan = value; } }
+    public int yasi { 
+    get { return yas; } set { yas = value; } }
+} }
